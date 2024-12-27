da05a62c06f9464c571c9cb6b141134a56d6cd81
Emerson Gomes Nunes <emerson100loko@hotmail.com>
2024-12-26 23:08:34 -0300

    BCrypt

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$6hTQnkU4DzW1XjM2URSrnuiAkWH8Z7EfWNEh12hXjBJX/ma3pLXqC');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$6hTQnkU4DzW1XjM2URSrnuiAkWH8Z7EfWNEh12hXjBJX/ma3pLXqC');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
